942325df4cdfae89f14aae2753471ea8bcebc67a
David Suwandi <davidsuwandi@gmail.com>
2025-07-02 16:03:00 +0700

    feat: add warga apis

---
+++ app/Http/Controllers/Api/WargaController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\Warga;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class WargaController extends Controller
+{
+  public function index()
+  {
+    return Warga::all();
+  }
+
+  public function store(Request $request)
+  {
+    $data = $request->validate([
+      'nama' => 'required|string|max:255',
+      'alamat' => 'required|string',
+      'no_hp' => 'nullable|string|max:20',
+      'tanggal_lahir' => 'nullable|date',
+      'jenis_kelamin' => 'required|in:L,P',
+      'status_keluarga' => 'nullable|string|max:255',
+      'is_kepala_keluarga' => 'boolean',
+    ]);
+
+    $warga = Warga::create($data);
+    return response()->json($warga, 201);
+  }
+
+  public function show($id)
+  {
+    return Warga::findOrFail($id);
+  }
+
+  public function update(Request $request, $id)
+  {
+    $warga = Warga::findOrFail($id);
+
+    $data = $request->validate([
+      'nama' => 'required|string|max:255',
+      'alamat' => 'required|string',
+      'no_hp' => 'nullable|string|max:20',
+      'tanggal_lahir' => 'nullable|date',
+      'jenis_kelamin' => 'required|in:L,P',
+      'status_keluarga' => 'nullable|string|max:255',
+      'is_kepala_keluarga' => 'boolean',
+    ]);
+
+    $warga->update($data);
+    return response()->json($warga);
+  }
+
+  public function destroy($id)
+  {
+    $warga = Warga::findOrFail($id);
+    $warga->delete();
+
+    return response()->json(null, 204);
+  }
+}
+
